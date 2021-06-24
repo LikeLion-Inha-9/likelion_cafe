2063c9ace89f71e7eea963a5b71a20085e4af51b
dlwnsgus07 <37991960+dlwnsgus07@users.noreply.github.com>
2021-06-25 01:54:27 +0900

    계절학기 포강하고싶다...
    일단 잠자고 내일생각해보자....ㅠㅠㅠ

--- Mooding/mainapp/models.py
+++ Mooding/mainapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Cafe(models.Model): # 카페 클래스
+    id = models.AutoField(primary_key=True) # 카페 아이디
+    title = models.CharField(max_length=100) # 카페이름
+    explanation = models.TextField() # 카페 설명
+    reservation_available = models.BooleanField() #예약 가능여부
+    charge_available = models.BooleanField() # 충전 가능 여부
+    total_seats = models.SmallIntegerField() # 총 좌석 수
+    used_seats = models.SmallIntegerField() # 사용하고 있는 좌석 수
+    unused_seats = models.SmallIntegerField() # 미사용 좌석 수
+    congestion_status = models.SmallIntegerField() # 혼잡여부(숫자료 표현 0, 1, 2)
+    lat = models.SmallIntegerField() #위도
+    lng = models.SmallIntegerField() #경도
+    thumbnail = models.ImageField(default ="#") #썸네일 이미지
+
+
+class Review(models.Model): # 리뷰 서비스
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE) # 카페 하나에 종속
+    writer = models.CharField(max_length=20)# 작성자
+    rating = models.SmallIntegerField() # 별점
+    comment = models.TextField() #리뷰내용
+
+
+class Product(models.Model): #판매할 상품
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE) # 카페 하나에 종속
+    title = models.CharField(max_length=100) #상품 이름
+    price = models.IntegerField() # 상품가격
+    out_of_stock = models.BooleanField() # 매진여부
+
+
+class Image(models.Model): # 이미지 
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE) #카페 종속
+    representative_image = models.ImageField() #카페 대표 이미지
--- Mooding/mainapp/views.py
+++ Mooding/mainapp/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.db import models
+from Mooding.mainapp.models import Cafe
+from django.shortcuts import get_object_or_404, redirect, render
 
 # Create your views here.
+
+def home(req):
+    cafe_objects = Cafe.objects.all()
+    return render(req, '#', {'data' : cafe_objects})
+
+def cafe_create(req):
+    if req.mathod == 'POST':
+        cafe_object = Cafe()
+        cafe_object.title = req.POST['title']
+        cafe_object.explanation = req.POST['explanation']
+        cafe_object.reservation_available = req.POST['reservation']
+        cafe_object.charge_available = req.POST['charge']
+        cafe_object.total_seats = req.POST['total_seats']
+        cafe_object.congestion_status = req.POST['congestion_status']
+        # 위치랑 경도 받아오는 거 작업하기.
+        cafe_object.thumbnail = req.POST['thumbnail']
+        cafe_object.save()
+        return redirect('/cafe/'+str(cafe_object.id))
+    return render(req, 'cafe_create.html')
+
+def cafe_read(req, id):
+    cafe_object = get_object_or_404(Cafe, pk=id)
+    reviews = cafe_object.review_set.all()
+    content = {
+        'data' : cafe_object,
+        'reviews' :  reviews,
+    }
+    return render(req, 'cafe.html', content)
+
+def cafe_edit(req, id):
+    cafe_object = get_object_or_404(Cafe, pk=id)
+    if req.mathod == 'POST':
+        cafe_object = Cafe()
+        cafe_object.title = req.POST['title']
+        cafe_object.explanation = req.POST['explanation']
+        cafe_object.reservation_available = req.POST['reservation']
+        cafe_object.charge_available = req.POST['charge']
+        cafe_object.total_seats = req.POST['total_seats']
+        cafe_object.congestion_status = req.POST['congestion_status']
+        # 위치랑 경도 받아오는 거 작업하기.
+        cafe_object.thumbnail = req.POST['thumbnail']
+        cafe_object.save()
+        cafe_object.save()
+        return redirect('/cafe/'+str(id))
+    return render(req, 'cafe_edit.html', {'data' : cafe_object})
+
+def cafe_delete(req, id):
+    cafe_object = get_object_or_404(Cafe, pk=id)
+    cafe_object.delete()
+    return redirect('/')
+
+def create_reivew(req, id):
+    if req.method == 'POST':
+        cafe_object = get_object_or_404(Cafe, pk=id)
+        user = User.objects.get(user=req.user)
+        cafe_object.reivew_set.create(rating = req.POST['rating'], comment = req.POST['comment'])
+        #여기서 회원 닉네임 가져오는 거 추가해서 처리하기
+    return redirect('/cafe/'+ str(id))
